5e74f3e414b23c08433f15625b6eb0a8ed7c7017
DominicMoser <dominic_moser@icloud.com>
2020-04-20 21:14:22 +0200

    Löschen was ich nicht brauche

--- install-shairport.sh
+++ install-shairport.sh
@@ -3,30 +3,15 @@
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
 echo
-echo -n "Do you want to install Shairport Sync AirPlay Audio Receiver (shairport-sync v${SHAIRPORT_VERSION})? [y/N] "
-read REPLY
-if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
-
-apt install --no-install-recommends -y avahi-daemon libavahi-client3 libconfig9 libdaemon0 libjack-jackd2-0 libmosquitto1 libpopt0 libpulse0 libsndfile1 libsoxr0
-dpkg -i files/shairport-sync_3.3.5-1~bpo10+1_armhf.deb
-usermod -a -G gpio shairport-sync
-raspi-config nonint do_boot_wait 0
-
-PRETTY_HOSTNAME=$(hostnamectl status --pretty)
-PRETTY_HOSTNAME=${PRETTY_HOSTNAME:-$(hostname)}
+echo "Installing Shairport Sync"
+cd ~/downloads/shairport-sync
+autoreconf -i -f
+./configure --with-stdout --with-avahi --with-ssl=openssl --with-metadata
+make
+sudo make install
 
 cat <<EOF > "/etc/shairport-sync.conf"
 general = {
   name = "${PRETTY_HOSTNAME}";
-}
-
-alsa = {
-//  mixer_control_name = "Softvol";
-}
-
-sessioncontrol = {
-  session_timeout = 20;
 };
 EOF
-
-systemctl enable --now shairport-sync
--- install.sh
+++ install.sh
@@ -1,10 +1,22 @@
 #!/bin/bash -e
 
+
+sudo apt-get update
+
+sudo apt install git uild-essential xmltoman autoconf automake libtool libdaemon-dev libpopt-dev libconfig-dev avahi-daemon libavahi-client-dev libssl-dev libmbedtls-dev libsoxr-dev libsndfile1-dev
+
+mkdir ~/downloads
+cd ~/downloads 
+
+git clone https://github.com/nicokaiser/rpi-audio-receiver
+git clone https://github.com/mikebrady/shairport-sync
+wget https://github.com/badaix/snapcast/releases/download/v0.19.0/snapserver_0.19.0-1_armhf.deb
+
 read -p "Hostname [$(hostname)]: " HOSTNAME
 sudo raspi-config nonint do_hostname ${HOSTNAME:-$(hostname)}
 
 CURRENT_PRETTY_HOSTNAME=$(hostnamectl status --pretty)
-read -p "Pretty hostname [${CURRENT_PRETTY_HOSTNAME:-Raspberry Pi}]: " PRETTY_HOSTNAME
+read -p "DisplayName [${CURRENT_PRETTY_HOSTNAME:-Raspberry Pi}]: " PRETTY_HOSTNAME
 sudo hostnamectl set-hostname --pretty "${PRETTY_HOSTNAME:-${CURRENT_PRETTY_HOSTNAME:-Raspberry Pi}}"
 
 echo "Updating packages"
@@ -14,10 +26,6 @@ sudo apt upgrade -y
 echo "Installing components"
 sudo ./install-bluetooth.sh
 sudo ./install-shairport.sh
-sudo ./install-spotify.sh
-sudo ./install-upnp.sh
 sudo ./install-snapcast-client.sh
 sudo ./install-startup-sound.sh
-sudo ./install-pivumeter.sh
-sudo ./enable-hifiberry.sh
-sudo ./enable-read-only.sh
+
